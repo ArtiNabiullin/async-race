290e985130195d57e67c527a3baea9dfa2c66f24
ArtiNabiullin <anabiullin05@gmail.com>
2026-07-28 23:58:22 +0500

    feat: creating a class for the machine form 28.07.2026 23:59

---
+++ src/components/CarForm.ts
@@ -0,0 +1,26 @@
+export class CarForm {
+  public render(): HTMLFormElement {
+    const form = document.createElement("form");
+    form.className = "d-flex gap-2 mb-4";
+
+    const nameInput = document.createElement("input");
+    nameInput.type = "text";
+    nameInput.placeholder = "Car name";
+    nameInput.required = true;
+    nameInput.className = "form-control";
+
+    const colorInput = document.createElement("input");
+    colorInput.type = "color";
+    colorInput.value = "#00000";
+    colorInput.className = "form-control form-control-color";
+
+    const createButton = document.createElement("button");
+    createButton.type = "submit";
+    createButton.className = "btn btn-success";
+    createButton.textContent = "Create";
+
+    form.append(nameInput, colorInput, createButton);
+
+    return form;
+  }
+}
